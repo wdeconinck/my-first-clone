b7b928c36944b81de824aad2f076ec46574ebe06
Willem Deconinck <willem.deconinck@ecmwf.int>
2020-09-15 11:07:49 +0100

    wip

--- tests/test_useful_things.py
+++ tests/test_useful_things.py
@@ -1,4 +1,6 @@
-import ..useful_things
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import useful_things
 from hypothesis import given
 import hypothesis.strategies as st
 
